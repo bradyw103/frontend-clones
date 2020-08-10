fb735a3e6af39dc184d728631695bdde45ae33b2
brkyozdemir <berkayozdemir02@gmail.com>
2020-08-10 19:20:44 +0300

    updates

--- youtube-clone/src/RecommendedVideos.js
+++ youtube-clone/src/RecommendedVideos.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import './RecommendedVideos.css'
+import VideoCard from './VideoCard';
 
 const RecommendedVideos = () => {
     return (
         <div className="recommendedVideos">
-            Selamlar
+            <h2>Recommended</h2>
+            <div className="recommendedVideos__videos">
+                {[0, 1, 2, 3, 4, 5].map(data => (<VideoCard
+                    title="Deneme"
+                    views="2.3M Views"
+                    timestamp="3 days ago"
+                    channelImage=""
+                    channel="Berkay Özdemir"
+                    image="https://i.ytimg.com/vi/0pThnRneDjw/hq720.jpg?sqp=-oaymwEZCNAFEJQDSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLCvK0Bi1naQJuqpiNqaST3zTtePQg"
+                />))}
+            </div>
         </div>
     )
 }
--- youtube-clone/src/Sidebar.js
+++ youtube-clone/src/Sidebar.js
@@ -3,6 +3,12 @@ import SidebarRow from './SidebarRow';
 import HomeIcon from '@material-ui/icons/Home';
 import WhatshotIcon from '@material-ui/icons/Whatshot';
 import SubscriptionsIcon from '@material-ui/icons/Subscriptions';
+import VideoLibraryIcon from '@material-ui/icons/VideoLibrary';
+import HistoryIcon from '@material-ui/icons/History';
+import OndemandVideoIcon from '@material-ui/icons/OndemandVideo';
+import WatchLaterIcon from '@material-ui/icons/WatchLater';
+import ThumbUpAltOutlinedIcon from '@material-ui/icons/ThumbUpAltOutlined';
+import ExpandMoreOutlinedIcon from '@material-ui/icons/ExpandMoreOutlined';
 import './Sidebar.css';
 
 const Sidebar = () => {
@@ -12,7 +18,13 @@ const Sidebar = () => {
             <SidebarRow icon={<WhatshotIcon className="sidebarRow__icon" />} title="Trending" />
             <SidebarRow icon={<SubscriptionsIcon className="sidebarRow__icon" />} title="Subscription" />
             <hr />
-            <SidebarRow />
+            <SidebarRow icon={<VideoLibraryIcon className="sidebarRow__icon" />} title="Library" />
+            <SidebarRow icon={<HistoryIcon className="sidebarRow__icon" />} title="History" />
+            <SidebarRow icon={<OndemandVideoIcon className="sidebarRow__icon" />} title="Your Videos" />
+            <SidebarRow icon={<WatchLaterIcon className="sidebarRow__icon" />} title="Watch Later" />
+            <SidebarRow icon={<ThumbUpAltOutlinedIcon className="sidebarRow__icon" />} title="Liked Videos" />
+            <SidebarRow icon={<ExpandMoreOutlinedIcon className="sidebarRow__icon" />} title="Show More" />
+            <hr />
         </div>
     )
 }
